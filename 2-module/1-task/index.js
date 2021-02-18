65a543c4f165fc0298da0a4bb83526c1f298d75b
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-02-18 12:04:17 +0300

    Added task Функция sumSalary(salaries)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,9 +1,3 @@
 function sumSalary(salaries) {
-  let sum = 0;
-  for (let key in salaries) {
-    if (isFinite(salaries[key])) {
-      sum += salaries[key];
-    }
-  }
-  return sum;
+  // ваш код...
 }
