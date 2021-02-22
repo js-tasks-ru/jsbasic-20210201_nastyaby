b1bcebbe49d2ee4f1b8609f78ed9552d2e20a048
NastyaBy <62007947+NastyaBy@users.noreply.github.com>
2021-02-22 11:22:53 +0300

    Найти min/max в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+  const arr = str.split(' ').join().split(',')
+
+  const filterArr = arr.filter(item => item !== '').map(item => parseFloat(item)).filter(item => !!item);
+
+  let obj = {};
+
+  obj.min = Math.min(...filterArr);
+  obj.max = Math.max(...filterArr);
+
+  return obj;
 }
