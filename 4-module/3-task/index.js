53fb54dee089c66769044a7b1838e1c1fe494478
NastyaBy <62007947+NastyaBy@users.noreply.github.com>
2021-02-23 12:34:36 +0300

    Функция highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,32 @@
 function highlight(table) {
   // ваш код...
+  const rowsTable = table.rows;
+
+  for (let i = 0; i < rowsTable.length; i++) {
+
+    const row = rowsTable[i];
+    const collStatus = row.lastElementChild;
+    const collGender = row.cells[2];
+    const collAge = row.cells[1];
+
+    const dataAvailable = collStatus.getAttribute(`data-available`);
+
+    if (dataAvailable === `true`) {
+      row.classList.add(`available`);
+    } else if (dataAvailable === `false`) {
+      row.classList.add(`unavailable`);
+    } else {
+      row.setAttribute('hidden', true);
+    }
+
+    if (collGender.textContent === `m`) {
+      row.classList.add(`male`);
+    } else if (collGender.textContent === `f`) {
+      row.classList.add(`female`);
+    }
+
+    if (collAge.textContent < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
 }
