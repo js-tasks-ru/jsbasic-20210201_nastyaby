9a55246d63ffaf1758ee99d612e097536c8f399b
NastyaBy <62007947+NastyaBy@users.noreply.github.com>
2021-03-11 15:20:19 +0300

    Таблица с удаляемыми строками

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,57 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+
+    let table = document.createElement('table');
+
+    table.innerHTML = `
+      <thead>
+          <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+          </tr>
+      </thead>`;
+
+    let row = this.rows.map(rowsItem => {
+      return `
+        <tr>
+          <td>${rowsItem.name}</td>
+          <td>${rowsItem.age}</td>
+          <td>${rowsItem.salary}</td>
+          <td>${rowsItem.city}</td>
+          <td><button>X</button></td>
+        </tr>
+      `;
+    }).join('');
+
+    table.innerHTML += `
+     <tbody>
+        ${row}
+      </tbody>
+    `;
+
+    table.addEventListener('click', (evt) => {
+      this.onDeleteRow(evt);
+    });
+
+    this._elem = table;
+  }
+
+  onDeleteRow(evt) {
+    if (evt.target.tagName !== 'BUTTON') {
+      return;
+    }
+
+    let tr = evt.target.closest('tr');
+    tr.remove();
+
+  }
+
+  get elem() {
+    return this._elem;
   }
 }
