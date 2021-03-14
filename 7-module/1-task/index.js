668fdcad488141987342375e5090842c224ac7f1
NastyaBy <62007947+NastyaBy@users.noreply.github.com>
2021-03-14 22:31:05 +0300

    Учебный проект: Лента-Меню.Прокрутка

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -23,5 +23,23 @@ export default class RibbonMenu {
                     <img src="../../assets/images/icons/angle-icon.svg" alt="icon">
                 </button>
             </div>`);
+
+    const ribbonArrowRight = this.elem.querySelector('.ribbon__arrow_right');
+    const ribbonArrowLeft = this.elem.querySelector('.ribbon__arrow_left');
+    const ribbonInner = this.elem.querySelector('.ribbon__inner');
+
+    ribbonArrowRight.addEventListener(`click`,
+      () => this.arrowRight(ribbonInner));
+    ribbonArrowLeft.addEventListener(`click`,
+      () => this.arrowLeft(ribbonInner));
   }
+
+  arrowRight(ribbonInner) {
+    ribbonInner.scrollBy(350, 0);
+  }
+
+  arrowLeft(ribbonInner) {
+    ribbonInner.scrollBy(-350, 0);
+  }
+
 }
