e68b08b1852a4316c273bf89f2cd3f127d244403
NastyaBy <62007947+NastyaBy@users.noreply.github.com>
2021-03-14 20:49:52 +0300

    Учебный проект: Лента-Меню. Отрисовка вёрстки

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -3,5 +3,25 @@ import createElement from '../../assets/lib/create-element.js';
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+    this.render();
+  }
+
+  render() {
+    this.elem = createElement(
+      `<div class="ribbon">
+                <button class="ribbon__arrow ribbon__arrow_left ribbon__arrow_visible">
+                    <img src="../../assets/images/icons/angle-icon.svg" alt="icon">
+                </button>
+
+                <nav class="ribbon__inner">
+                     ${this.categories.map((item) => `
+                        <a href="#" class="ribbon__item" data-id="${item.id}">${item.name}</a>
+                     `).join('')}
+                </nav>
+
+                <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
+                    <img src="../../assets/images/icons/angle-icon.svg" alt="icon">
+                </button>
+            </div>`);
   }
 }
